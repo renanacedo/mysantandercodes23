21c837f6a94514f993a59ab440c7eef9b6c118de
Renan Acedo <renan.acedoo@gmail.com>
2023-11-21 18:39:42 -0300

    "class 8"

--- java/javaSantanderCoders/src/Main.java
+++ java/javaSantanderCoders/src/Main.java
@@ -1,6 +1,8 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.TextStyle;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Main {
@@ -65,6 +67,35 @@ public class Main {
 
 
         // Vetores
+        String[] cadaDiaSemana = new String[7];
 
+        for (int i = 0; i < 7; i++){
+            String diasSemana = hoje.getDayOfWeek() .getDisplayName(TextStyle.FULL, brasil);
+            diasSemana = diasSemana.substring(0,1).toUpperCase(brasil) + diasSemana.substring(1);
+            cadaDiaSemana[i] = diasSemana;
+            hoje = hoje.plusDays(1);
+        }
+        for (String dia : cadaDiaSemana) {
+            System.out.println(dia);
+        }
+
+        // Encontrando o maior numero, o menor e a média
+        int[] numeros = {9, 10, 12, 25, 2};
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
+
+        for (int i=0; i < numeros.length; i++){
+            if (numeros[i] > maior){
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+            media += numeros[i];
+        }
+        System.out.println("\nMaior " + maior);
+        System.out.println("Menor " + menor);
+        System.out.println("Media " + media/numeros.length);
     }
 }
